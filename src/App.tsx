88d01fc8c5cf820331b058364f152d6d7462a059
Matthieu Bleichner <matthieu258@hotmail.com>
2024-02-19 12:46:27 +0100

    chore(projectSetup): add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import { DragDropContext } from "react-beautiful-dnd";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
